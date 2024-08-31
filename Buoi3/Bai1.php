2de61ab9c9c1f78be9962458e430dfee4778d4d3
unknown <vuthuong410@gmail.com>
2024-08-31 22:16:40 +0700

    first commit

--- Buoi3/Bai1.php
+++ Buoi3/Bai1.php
@@ -6,98 +6,121 @@
     <title>Document</title>
 </head>
 <style>
-body {
-    color: #49108B;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    height: 100vh;
-    background-color: #F3F8FF;
-    font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-}
-form{
-    background-color: #F3F8FF;
-    display: flex;
-    flex-direction: column;
-    padding: 40px;
-    border: 2px solid #49108B;
-    box-shadow: 2px 2px 10px #7d30e17a;
-    border-radius: 10px;
-    position: relative;
-}
-h2{
-    text-align: center;
-}
-td, th {
-  padding: 14px 6px;
-}
-tr td:first-child{
-    text-align: right;
-    color: #49108B;
-}
-label {
-    font-size: 18px;
-    margin-bottom: 5px; 
-    display: block;
-}
-input{
-    border-radius: 5px;
-    border: 1px solid #49108B;
-}
-input:focus{
-    outline: solid #49108B;
-}
-input[type="text"], input[type="number"], input[type="mail"] {
-    padding: 5px;
-}
-input[type="submit"] {
-    color: #F3F8FF;
-    grid-column: span 2;
-    height: 36px;
-    background-color: #49108B;
-    border: none;
-    border-radius: 10px;
-    font-weight: bold;
-    margin-bottom: 5px;
-    margin-top: 10px;
-}
-input[type="submit"]:hover{
-    background-color: #9448ea;
-    transition: 0.4s;
-}
-span.error{
-    position: absolute;
-    padding-top: 2px;
-    font-size: 12px;
-    color: #ff0000;
-}
-#err{
-    color: #ff0000;
-    text-align: center;
-    position: absolute;
-    left: 50%;
-    bottom: 3%;
-    transform: translate(-50%,-50%);
-    width: 100%;
-}
-button{
-    color: #F3F8FF;
-    grid-column: span 2;
-    height: 36px;
-    background-color: #49108B;
-    border: none;
-    border-radius: 10px;
-    font-weight: bold;
-    margin-top: 20px;
-    width: 100%;
-}
-button:hover{
-    background-color: #9448ea;
-    transition: 0.4s;
-}
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+    }
+
+    body {
+        color: #49108B;
+        background-color: #F3F8FF;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        overflow: hidden;
+    }
+
+    form {
+        display: flex;
+        flex-direction: column;
+        padding: 40px;
+        border: 2px solid #49108B;
+        box-shadow: 2px 2px 10px #7d30e17a;
+        border-radius: 10px;
+        background-color: #F3F8FF;
+        position: relative;
+    }
+
+    h2 {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    table {
+        width: 100%;
+    }
+
+    td, th {
+        padding: 14px 6px;
+        font-size: 18px;
+    }
+
+    tr td:first-child {
+        text-align: right;
+        color: #49108B;
+        font-weight: bold;
+    }
+
+    label {
+        font-size: 18px;
+        margin-bottom: 5px;
+        display: block;
+    }
+
+    input[type="text"], input[type="number"], input[type="mail"] {
+        width: 100%;
+        padding: 5px;
+        border: 1px solid #49108B;
+        border-radius: 5px;
+    }
+
+    input[type="text"]:focus, input[type="number"]:focus, input[type="mail"]:focus {
+        outline: solid #49108B;
+    }
+
+    input[type="submit"] {
+        color: #F3F8FF;
+        background-color: #49108B;
+        border: none;
+        border-radius: 10px;
+        font-weight: bold;
+        height: 36px;
+        margin: 10px 0 5px;
+        cursor: pointer;
+        transition: background-color 0.4s;
+    }
+
+    input[type="submit"]:hover {
+        background-color: #9448ea;
+    }
+
+    span.error {
+        position: absolute;
+        padding-top: 2px;
+        font-size: 12px;
+        color: #ff0000;
+    }
+
+    #err {
+        color: #ff0000;
+        text-align: center;
+        position: absolute;
+        left: 50%;
+        bottom: 3%;
+        transform: translate(-50%, -50%);
+        width: 100%;
+    }
+
+    button {
+        color: #F3F8FF;
+        background-color: #49108B;
+        border: none;
+        border-radius: 10px;
+        font-weight: bold;
+        height: 36px;
+        margin-top: 20px;
+        width: 100%;
+        cursor: pointer;
+        transition: background-color 0.4s;
+    }
+
+    button:hover {
+        background-color: #9448ea;
+    }
+
 </style>
 <body>
     <?php
